e34a589d6540d7db8f319911b3963ef81671205f
stones <comujs@gmail.com>
2025-08-09 12:24:36 +0900

    msg3

--- src/About.jsx
+++ src/About.jsx
@@ -1,8 +1,18 @@
+
 function About() {
   
+    function btn1() {
+      console.log("로그1");
+    }
+
+    const btn2 = () => {
+      console.log("로그2");
+    }
     return (
       <>
         <p>Hello About!</p>
+        <p><button onClick={btn1}>버튼111</button></p>
+        <p><button onClick={() => btn2()}>버튼222</button></p>
       </>
     )
   }
